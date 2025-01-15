5f10efd167dbd4774107eca763031eb3ffa2c7e4
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2025-01-15 15:04:49 -0300

    *

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import cv2
 import pytesseract
 
-imagem = cv2.imread('Fotografia - Comprovante de Entrega - Mobile - mobile.jpg')
+
+imagem = cv2.imread('Screenshot_1.jpg')
 
 pytesseract.pytesseract.tesseract_cmd= r'C:\Users\vitorgomes\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
 text = pytesseract.image_to_string(imagem,lang="por")
 
 print(text)
+
+
